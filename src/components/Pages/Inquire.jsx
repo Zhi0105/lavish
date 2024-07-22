ce2e2caeed54351be7bcfb0c4c90cad468f60531
Argie <argie.b@flowerstore.ph>
2024-07-22 23:22:45 +0800

    fix form with on mobile phone

--- src/components/Pages/Inquire.jsx
+++ src/components/Pages/Inquire.jsx
@@ -27,9 +27,9 @@ export const Inquire = () => {
 
   return (
     <div className="inquire_main flex min-h-screen flex-col items-center justify-center mx-4">
-      <section className="bg-gray-50 w-1/2 rounded-lg shadow-lg flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0 my-12">
+      <section className="bg-gray-50 xs:w-full sm:w-full md:w-4/5 lg:w-1/2 xl:w-1/2 rounded-lg shadow-lg flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0 my-12">
         <Logo height={200} width={200} />        
-        <h1 className="mt-5 font-bold text-2xl">Submit Inquiries</h1>
+        <h1 className="mt-5 font-bold text-2xl">Personal Information</h1>
         <div className="form_container w-full p-6 space-y-4 md:space-y-6 sm:p-8">
           <div className="form space-y-4 md:space-y-6">
             <div className="name_textfield">
@@ -45,7 +45,7 @@ export const Inquire = () => {
                       onChange={onChange}
                       type="name" 
                       name="name" id="name" 
-                      placeholder="name" 
+                      placeholder="Full name" 
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" 
                     />
                     
@@ -69,7 +69,7 @@ export const Inquire = () => {
                       onChange={onChange}
                       type="address" 
                       name="address" id="address" 
-                      placeholder="address" 
+                      placeholder="Address" 
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" 
                     />
                     
@@ -93,7 +93,7 @@ export const Inquire = () => {
                       onChange={onChange}
                       type="mobile" 
                       name="mobile" id="mobile" 
-                      placeholder="mobile" 
+                      placeholder="Mobile number" 
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" 
                     />
                     
@@ -117,7 +117,7 @@ export const Inquire = () => {
                   onChange={onChange}
                   type="email" 
                   name="email" id="email" 
-                  placeholder="email" 
+                  placeholder="Email" 
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" 
                 />
                 )}
@@ -139,7 +139,7 @@ export const Inquire = () => {
                       onChange={onChange}
                       type="service" 
                       name="service" id="service" 
-                      placeholder="service" 
+                      placeholder="Service" 
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" 
                     />
                     
